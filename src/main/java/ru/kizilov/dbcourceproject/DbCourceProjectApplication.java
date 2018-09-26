c158fb6a68c134faba8b9f5a6c4f419241b39e36
nilke1995 <brekhin@bk.ru>
2018-09-27 02:04:41 +0300

    empty project was init

---
+++ src/main/java/ru/kizilov/dbcourceproject/DbCourceProjectApplication.java
@@ -0,0 +1,12 @@
+package ru.kizilov.dbcourceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbCourceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbCourceProjectApplication.class, args);
+	}
+}
